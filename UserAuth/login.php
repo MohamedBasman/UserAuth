231bd1ea4a2d3bf33de170c47b593388efbc0119
MohamedBasman <210701160@rajalakshmi.edu.in>
2024-07-14 11:30:12 +0530

    Initial commit

---
+++ UserAuth/login.php
@@ -0,0 +1,24 @@
+<?php
+require 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            echo "Login successful";
+        } else {
+            echo "Invalid credentials";
+        }
+    } else {
+        echo "No user found";
+    }
+
+    $conn->close();
+}
+?>
